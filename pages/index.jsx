2d6dc76219359c1db99df22e64ec853d8b7d870b
Casuso <albertofcasuso@gmail.com>
2022-07-14 15:55:38 +0200

    toast

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import Loader from '../components/Loader'
+import { Toaster, toast } from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div>
+      <Toaster/>
       <Loader show/>
+      <button onClick={()=>toast.success('hello toast')}>Toast me</button>
     </div>
   )
 }
