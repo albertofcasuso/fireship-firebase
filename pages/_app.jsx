6f956ee6d660cb5cb9313fb72653afc8d9d4e6aa
Casuso <albertofcasuso@gmail.com>
2022-07-15 16:00:59 +0200

    hasta auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,34 @@
-import '../styles/globals.css'
-import Navbar from '../components/Navbar'
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import { UserContext } from "../lib/context";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, userRef,userSnapshot } from "../lib/firebase";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return(
-  <>
-    <Navbar />
-    <Component {...pageProps} />
-  </>
-  )
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    let unsubscribe;
+    if (user) {
+      console.log(user.uid)
+      const ref = userRef(user.uid);
+      unsubscribe = userSnapshot(ref,(doc)=>{
+        console.log(doc.data())
+      })
+    } else {
+      setUsername(null);
+    }
+    return unsubscribe;
+  }, [user]);
+
+  return (
+    <UserContext.Provider value={{ user, username }}>
+      <Navbar />
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/enter.jsx
+++ pages/enter.jsx
@@ -1,8 +1,41 @@
+import { SignInWithPopup,SignOut } from "../lib/firebase"
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
 export default function Enter({}) {
+  const{user,username} = useContext(UserContext)
   
   return (
-    <div>
-        <h1>Admin site</h1>
-    </div>
+    <main>
+    {user ? 
+      !username ? <UsernameForm /> : <SignOutButton /> 
+      : 
+      <SignInButton />
+    }
+  </main>
   )
+}
+
+function SignInButton(){
+  const signInWithGoogle = async () =>{
+    await SignInWithPopup()
+  }
+  return(
+    <button className="btn-google" onClick={signInWithGoogle}>
+      Sign in with Google
+    </button>
+  )
+}
+
+
+function SignOutButton(){
+  return(
+    <button className="btn-google" onClick={SignOut}>
+      Sign Out
+    </button>
+  )
+}
+
+function UsernameForm() {
+  return null;
 }
